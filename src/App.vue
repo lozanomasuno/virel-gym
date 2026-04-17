fa042ae1c20b12876334d1da8212294db770f18e
Nelson <nlozanoc@emeal.nttdata.com>
2026-04-17 16:55:57 -0500

    feat: configuracion inicial del proyecto Vite + Vue 3 + Pinia + Vue Router

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+// NavBar y Footer ahora son responsabilidad de PublicLayout.vue
+// Las rutas privadas usan AdminLayout.vue y MemberLayout.vue
+</script>
+
+<template>
+  <RouterView />
+</template>
+
